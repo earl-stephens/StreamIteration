ab6690bc07fdd422c43563b466952de451cb9042
earl-stephens <stephensearl@hotmail.com>
2021-12-22 17:21:01 -0700

    Show example of streams in arrays class

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
 
@@ -15,8 +16,37 @@ public class App {
 		Arrays.stream(animals).forEach(animal -> {
 			System.out.println(animal);
 		});
+		/* Coding challenge
+		 * Write a program that gets three numbers input by the user (keep 
+		 * asking until they have entered exactly 3 numbers),
+		 * prints the numbers entered, prints the total of the numbers.
+		 * Use a stream for iteration.
+		 */
 
-
+		Scanner scanner = new Scanner(System.in);
+		final int DELIMITER = 3;
+		int[] userInput = new int[DELIMITER];
+		
+		System.out.println("Enter three integers below:");
+		
+		for(int i = 0; i < DELIMITER; i++) {
+			System.out.println("Enter the integer: ");
+			userInput[i] = scanner.nextInt();
+		}
+		scanner.close();
+		
+		int total = 0;
+		System.out.println("The numbers you entered are:");
+		
+		Arrays.stream(userInput).forEach(num -> {
+			System.out.println(num);
+			//total = total + num;
+		});
+		
+		for(int j = 0; j < userInput.length; j++) {
+			total += userInput[j];
+		}
+		System.out.printf("The sum total is %d", total);
 	}
 
 }
